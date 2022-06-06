93aa67da7631f50843d0acb3d4a608acfe2309ce
Cait Matthews <caitmatthews@Caitlins-MacBook-Pro.local>
2022-06-06 15:59:09 -0400

    organizing exercises with corresponding console.logs

--- app.js
+++ app.js
@@ -3,17 +3,26 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1 - 2
+// Exercise 1 and 2
 
-const firstName = "Cait";
+const firstName = "Cait"; 
 let lastName = "Matthews";
 var age = 24;
+
+lastName = "Matthews";
+age = 25;
+
+console.log ("My name is", firstName, lastName)
+console.log ("I will be", age, "years old in November")
 
 // Exercise 3
 
 let language = "JavaScript";
 let createdYear = 1995;
 let isCaseSensitive = true;
+
+console.log (language, "was created in", createdYear)
+console.log ("Is", language, "case sensitive?", isCaseSensitive)
 
 // Exercise 4
 
@@ -25,6 +34,10 @@ let stock = 0;
 let inStock = false;
 let selectedSize = "M";
 
+console.log (`${productName} is priced at ${price}.`)
+console.log ("there are", stock, selectedSize, "t-shirts left")
+console.log ("Is the product in stock?", inStock)
+
 // Exercise 5
 
 let title = "Name of the Wind";
@@ -33,13 +46,6 @@ let pageCount = 722;
 let bookmark = 456;
 let hasRead = true;
 
-console.log ("My name is", firstName, lastName)
-console.log ("I am", age, "years old")
-console.log (language, "was created in", createdYear)
-console.log ("Is", language, "case sensitive?", isCaseSensitive)
-console.log (`${productName} is priced at ${price}`)
-console.log ("there are", stock, selectedSize, "t-shirts left")
-console.log ("Is the product in stock?", inStock)
 console.log (title, "is written by", author, "and is", pageCount, "pages long")
 console.log ("I am on page", bookmark)
 console.log ("Have I read it before?", hasRead)
