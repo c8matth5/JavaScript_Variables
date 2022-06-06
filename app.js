e65ae8abe19f9e5ae2f139146d9593515121b6e5
Cait Matthews <caitmatthews@Caitlins-MacBook-Pro.local>
2022-06-06 15:09:09 -0400

    JavaScript

--- app.js
+++ app.js
@@ -3,8 +3,44 @@ console.log(
   "Follow the steps in the README.md file to complete the exercises:\n==========\n"
 );
 
-// Exercise 1
+// Exercise 1 - 2
 
-// Exercise 2
+const firstName = "Cait";
+let lastName = "Matthews";
+var age = 24;
 
 // Exercise 3
+
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+// Exercise 4
+
+let productName = "t-shirt"
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+// Exercise 5
+
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log ("My name is", firstName, lastName)
+console.log ("I am", age, "years old")
+console.log (language, "was created in", createdYear)
+console.log ("Is", language, "case sensitive?", isCaseSensitive)
+console.log (`${productName} is priced at ${price}`)
+console.log ("there are", stock, selectedSize, "t-shirts left")
+console.log ("Is the product in stock?", inStock)
+console.log (title, "is written by", author, "and is", pageCount, "pages long")
+console.log ("I am on page", bookmark)
+console.log ("Have I read it before?", hasRead)
+
